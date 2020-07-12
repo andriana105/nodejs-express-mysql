a12e8d0e91e5038b09cab4e9637bed03f3d67e5d
andriana <andrianamarisa@gmail.com>
2020-07-13 01:05:20 +0700

    first commit

---
+++ app/controllers/customer.controller.js
@@ -0,0 +1,122 @@
+const Customer = require("../model/customers.model");
+
+// Create and Save a new customer
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    // Create customer
+    const customer = new Customer({
+        email: req.body.email,
+        name: req.body.name,
+        active: req.body.active
+    })
+
+    // Save Customer in the database
+    Customer.create(customer, (err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Customer."
+            });
+        else res.send(data);    
+    })
+
+};
+
+// Retrieve All Customers From The Database
+exports.findAll = (req, res) => {
+    Customer.getAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving customers."
+                });
+        else res.send(data);        
+    });
+};
+
+// Find one customer with a customerId
+exports.findOne = (req, res) => {
+    Customer.findById(req.params.customerId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Customer with ${req.params.customerId}`
+                });
+            }
+            else {
+                res.status(500).send({
+                    message: "Error retrieving Customer with id " + req.params.customerId
+                });
+            }
+        }
+        else res.send(data);
+    });
+};
+
+// Update a customer identified by the customerId in the request
+exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body){
+        res.status(400).send({
+            message: "Content can not be empty!"
+        })
+    }
+
+    Customer.updateById(
+        req.params.customerId,
+        new Customer(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "not_found"){
+                    res.status(404).send({
+                        messag: `Not found Customer with id ${req.params.customerId}.`
+                    })
+                }
+                else {
+                    res.status(500).send({
+                        message: "Error updating Customer with id " + req.params.customerId
+                    })
+                }
+            } else res.send(data);
+        }
+    )
+};
+
+// Delete a customer with the specified customerId in the request
+exports.delete = (req, res) => {
+    Customer.remove(req.params.customerId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found customer with id ${req.params.customerId}`
+                });
+            }
+            else {
+                res.status(500).send({
+                    message: "Could not delete Customer with id " + req.params.customerId
+                })
+            }
+        }
+        else res.send({message: `Customer was deleted successfully`})
+    });
+};
+
+// Delete All Customers From the Database
+exports.deleteAll = (req, res) => {
+    exports.deleteAll = (req, res) => {
+        Customer.removeAll((err, data) => {
+            if (err)
+                res.status(500).send({
+                    message:
+                        err.message || "Some error occurred while removing all customers."
+                });
+            else res.send({ message: `All customers were deleted successfully!`})    
+        });
+    };
+}
